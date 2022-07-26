92fe40ed076720ab8da1141f3e6b7acec7c04a91
curso <curso@curso.com>
2022-07-26 19:58:50 -0300

    adicionei mais conversas

--- app/src/main/java/br/com/impacta/recyclerviewdatabinding/HomeFragment.kt
+++ app/src/main/java/br/com/impacta/recyclerviewdatabinding/HomeFragment.kt
@@ -27,7 +27,16 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val colecaoConversas = mutableListOf<Conversa>(
-            Conversa("Fernando", "Olá, Fernando", "22:51", R.drawable.imagem_01)
+            Conversa("Fernando", "Olá, Fernando", "22:51", R.drawable.imagem_01),
+            Conversa("Raquel", "Olá, Raquel", "19:54", R.drawable.imagem_02),
+            Conversa("Cainã", "Olá, Cainã", "19:53", R.drawable.imagem_03),
+            Conversa("Alexandre", "Olá, Alexandre", "19:52", R.drawable.imagem_04),
+            Conversa("Diego", "Olá, Diego", "19:51", R.drawable.imagem_05),
+            Conversa("José Carlos", "Olá, José Carlos", "19:50", R.drawable.imagem_06),
+            Conversa("Rubiane", "Olá, Rubiane", "19:49", R.drawable.imagem_07),
+            Conversa("Vitor", "Olá, Vitor", "19:48", R.drawable.imagem_08),
+            Conversa("Luana", "Olá, Luana", "19:47", R.drawable.imagem_09),
+            Conversa("Impacta", "Olá, Impacta", "19:46", R.drawable.imagem_10),
         )
 
         binding.recyclerView.adapter = Adaptador(colecaoConversas)
